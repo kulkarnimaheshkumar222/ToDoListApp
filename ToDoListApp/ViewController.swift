3e534409755182dd63523d80b5a5341719827551
Mac <mac@macs-macbook-pro.local>
2018-10-03 11:21:37 +0530

    To do list complete with datasource and delegate methods

--- ToDoListApp/ViewController.swift
+++ ToDoListApp/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+var itemArray = ["To buy Book","Buy Notebook","Go To market"]
+    @IBOutlet weak var toDoListTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return  itemArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier:"cell" , for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        
+       
+        if (cell?.accessoryType == UITableViewCellAccessoryType.none)
+        {
+            cell?.accessoryType = .checkmark
+        }
+        else{
+            cell?.accessoryType = .none
+        }
+//        let selectedcell = cell?.textLabel?.text
+//        print("You selected : \(selectedcell!)")
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+        
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
